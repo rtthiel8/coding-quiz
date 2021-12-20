3672309a6d3bc2cd0ac7e28c38da1b2806a289ca
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2021-12-20 16:06:14 -0600

    setup html css js

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+
+document.getElementById('start').addEventListener('click', handleClick);
+
+function handleClick() {
+    document.getElementById('banner').style.display = 'none';
+    document.getElementById('questions').style.display = 'block';
+}
