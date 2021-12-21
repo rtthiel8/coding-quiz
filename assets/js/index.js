300947c49440e7c11d35df52a2fe1089257a84c5
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2021-12-21 17:05:47 -0600

    show questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,25 @@
-var totalQuestions = $('questions').size();
+//var totalQuestions = $('questions').size();
 
-var currentQuestion = 0;
+//var currentQuestion = 0;
 
-$questions = $('.questions');
+//$questions = $('.questions');
 
-$questions.hide();
+//$questions.hide();
 
-$(questions.get(currentQuestion)).fadeIn();
+//$(questions.get(currentQuestion)).fadeIn();
 
-
-document.getElementById('start').addEventListener('click', handleClick);
+document.getElementById('start').addEventListener('click', startQuiz);
+document.getElementById('start').addEventListener('click', showQuestion);
 
 function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-        seconds = parseInt(timer % 60,10);
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
+var timer = duration, seconds;
+setInterval(function () {
+    seconds = parseInt(timer % 60,10);
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = seconds;
+if (--timer < 0) {
+    timer = duration;
+    }
     }, 1000);
 }
 
@@ -31,7 +29,12 @@ window.onload = function () {
         startTimer(oneMinute, display);
 };
 
-function handleClick() {
+
+function startQuiz() {
     document.getElementById('banner').style.display = 'none';
+}
+
+function showQuestion () {
     document.getElementById('questions').style.display = 'block';
 }
+
