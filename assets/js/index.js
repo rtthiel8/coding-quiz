76f8804dee0d356dba6aa75b2706a2238424b111
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2021-12-21 14:21:39 -0600

    timer function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,35 @@
+var totalQuestions = $('questions').size();
+
+var currentQuestion = 0;
+
+$questions = $('.questions');
+
+$questions.hide();
+
+$(questions.get(currentQuestion)).fadeIn();
+
 
 document.getElementById('start').addEventListener('click', handleClick);
+
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60,10);
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinute = 60,
+        display = document.querySelector('#time');
+        startTimer(oneMinute, display);
+};
 
 function handleClick() {
     document.getElementById('banner').style.display = 'none';
