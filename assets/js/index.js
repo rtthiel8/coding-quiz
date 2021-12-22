b643b8ce88524297ac169e609fb9c1c4b1372de9
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2021-12-21 19:16:44 -0600

    questions into array

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,62 @@
-//var totalQuestions = $('questions').size();
-
-//var currentQuestion = 0;
-
-//$questions = $('.questions');
-
-//$questions.hide();
-
-//$(questions.get(currentQuestion)).fadeIn();
-
 document.getElementById('start').addEventListener('click', startQuiz);
-document.getElementById('start').addEventListener('click', showQuestion);
+//document.getElementById('start').addEventListener('click', showQuestion);
+
+//var quizContainer = document.getElementById('quiz');
+
+
+
+var quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers"
+        },
+        correctAnswer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _______.",
+        answers: {
+            1: "quotes",
+            2: "curly brackets",
+            3: "parentheses",
+            4: "square brackets",
+        },
+        correctAnswer: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above",
+        },
+        correctAnswer: "all of the above"
+    },
+    {
+        question: "String valuse must be enclosed within _______ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parentheses",
+        },
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is _______.",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log",
+        },
+        correctAnswer: "console.log"
+    }
+];
 
 function startTimer(duration, display) {
 var timer = duration, seconds;
@@ -32,9 +79,16 @@ window.onload = function () {
 
 function startQuiz() {
     document.getElementById('banner').style.display = 'none';
+    document.getElementById('question').style.display = 'block';
 }
 
-function showQuestion () {
-    document.getElementById('questions').style.display = 'block';
-}
+//function showQuestion () {
 
+//var output = [];
+
+//quizQuestions.forEach(
+    //(currentQuestion, questionNumber) => {
+
+        //var answers = [];
+
+        //for(number in currentQuestion.answers){
